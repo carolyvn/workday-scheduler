f49484e8ca47bbaab43a07a4a9186c536e341b81
Carol Yan <dreamfx@hotmail.com>
2022-01-25 23:44:20 -0800

    add timeblock to the document

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Display today's date and time
+var currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+$("#currentDay").text(currentDay);
+
+
+
+var saveBtn = $(".savebtn");
+
+
+saveBtn.on("click", function(){
+   
+});
