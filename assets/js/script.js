3073ef08c38355ec671b2ca032fb8c8ce32a6eed
Carol Yan <dreamfx@hotmail.com>
2022-02-16 17:58:14 -0800

    updated js with function to save values to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,27 @@ var currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentDay);
 
 
+var saveBtn = $(".saveBtn");
 
-var saveBtn = $(".savebtn");
-
-
-saveBtn.on("click", function(){
-   
+// Add event listener to save value to local storage
+saveBtn.on("click", function() {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time,text);
 });
+
+// Display value from local storage
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
+
+
